66570010e40103b4e50a28781e404d4691815d44
Danny Lopez <dalos00@gmail.com>
2020-07-09 16:15:00 -0500

    Aculiazion de codigo, incorporando JavaScript

--- js/funciones.js
+++ js/funciones.js
@@ -2,3 +2,29 @@ function abrirAlerta() {
     alert("Hola esto es un alert!!");
     console.log(5 + 10);
 }
+
+function login() {
+    var mensaje = document.getElementById('mensaje')
+    var usuario = document.getElementById('textUser');
+    var contrasena = document.getElementById('txtPass');
+
+    console.log(usuario.value + " " + contrasena.value);
+
+    if (usuario.value == "admin" && contrasena.value == "admin") {
+        mensaje.innerHTML = "Ok. Usuario logueado.";
+        location.href = "pagina2.html";
+    }
+    else {
+        mensaje.innerHTML = "Usuario no logueado.";
+        location.href = "pagina1.html";
+    }
+}
+
+function hacerAlgo() {
+    var reloj = document.getElementById('reloj');
+    reloj.innerHTML = new Date();
+}
+
+//hacerAlgo();
+
+setInterval(hacerAlgo, 1000)
